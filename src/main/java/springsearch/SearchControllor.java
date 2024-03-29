deabb5726b8a164fe943cf71010c36d9bcc6ad3e
Shruti Chille <2021bcs504@sggs.ac.in>
2024-03-29 21:05:41 +0530

    first commit

---
+++ src/main/java/springsearch/SearchControllor.java
@@ -0,0 +1,28 @@
+package springsearch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchControllor {
+	
+	//this is for showing the form
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	//this is redirect to google
+	@RequestMapping("/search")
+	public RedirectView search(@RequestParam("querybox") String query) {
+		
+		String url = "https://www.google.com/search?q="+query;
+		
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(url);
+		return redirectView;
+	}
+	
+}
